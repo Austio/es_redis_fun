147ef49b0c559edc6da8f5fde215236fd273b111
Austin Story <lonnieastory@gmail.com>
2018-03-18 17:22:44 -0500

    Net HTTP to request to 9200

--- Gemfile
+++ Gemfile
@@ -4,5 +4,6 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.4.1'
 
 gem 'sinatra', '~> 2.0.1'
+gem 'sinatra-contrib', '~> 2.0.1'
 gem 'sqlite3'
-# gem 'puma', '~> 3.11'
+gem 'pry'
--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'net/http'
+require 'pry'
 
 get '/' do
   'Howdy!'
+end
+
+get '/es' do
+  uri = URI.parse('http://localhost:9200')
+  h = Net::HTTP.new uri.host, uri.port
+  req = Net::HTTP::Get.new '/'
+
+  res = h.request req
+  res.body
 end
