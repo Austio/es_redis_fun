2ed04b8dfe577bfaeea1535156426e92840c523a
Austin Story <lonnieastory@gmail.com>
2018-03-19 22:36:47 -0500

    Use NET HTTP to request data from es

--- app.rb
+++ app.rb
@@ -3,11 +3,11 @@ require 'sinatra/reloader'
 require 'net/http'
 require 'pry'
 
-
 module ESUtils
-  ES_HOST =
-  def es_get(path = '/',)
-    Net::HTTP.get('http://localhost:9200', path)
+  def es_get(path = '/')
+    uri = URI("http://localhost:9200#{path}")
+
+    Net::HTTP.get_response(uri).body
   end
 end
 
